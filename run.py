c17f1c5d45cc096de068b6078e4da14b22f3783a
rocsteady <ccheney@pdx.edu>
2012-08-27 14:33:12 -0700

    Added python runaway program.

---
+++ run.py
@@ -0,0 +1,14 @@
+#test
+import subprocess
+import re
+
+processes = subprocess.check_output ( ['ps','-ef'] )
+lines = processes.split( '\n' )
+for line in lines:
+        process = re.split( '\s+', line )
+        # process[0] is the user
+        if process[0] == 'root':
+                continue
+        # TODO other filtering criteria here
+        print line
+
